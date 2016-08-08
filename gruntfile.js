2852a25f7f4cd7ba0fbf91470581cce7f41248b0
Dorotka <Panda-Home@Dorotkas-iMac.home>
2016-08-08 22:26:17 +0200

    added live reload task to grunt

--- gruntfile.js
+++ gruntfile.js
@@ -27,14 +27,31 @@ module.exports= function(grunt){
         }//distribution
       },// sass task
 
+      connect: {
+        server: {
+          options:{   
+               hostname: 'localhost',
+               port: 3000,
+               base: 'builds/development/',
+               livereload: true
+          }// options
+        }//server    
+      },//connect task
+
       watch: {
-         options: {
+        gruntfile: {
+          files: "gruntfile.js",
+          tasks: ['jshint:gruntfile']
+        },
+        options: {
             spawn: false
          },
          scripts: {
             files: ['contents/javascript/*.js',
                     'contents/sass/*.scss',
-                    'development/html/*.html'
+                    'development/css/*.css',
+                    'development/html/*.html',
+                    '!development/libraries/*js'
                    ],// files
             tasks: ['concat', 'sass']//tasks       
          }
@@ -45,10 +62,11 @@ module.exports= function(grunt){
 
 grunt.loadNpmTasks('grunt-contrib-concat'); // grunt concat
 grunt.loadNpmTasks('grunt-contrib-sass'); // grunt sass
+grunt.loadNpmTasks('grunt-contrib-connect'); // grunt sass
 grunt.loadNpmTasks('grunt-contrib-watch'); // grunt watch
 
 // grunt default tasks
-grunt.registerTask('default',['concat','sass', 'watch']); 
+grunt.registerTask('default',['concat','sass', 'connect', 'watch']); 
 };// wrapper function
 
 }()); // use strict function 
